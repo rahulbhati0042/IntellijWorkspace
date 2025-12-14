89be087616178de8d97d262138296651dae97711
Rahul Bhati <rahulbhati0042@gmail.com>
2025-12-14 22:59:43 +0530

    Initial commit for spring security fundamentals with Get API Hello.

---
+++ spring-security-fundamentals/src/main/java/com/springsecurity/controller/MainApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(MainApplication.class);
+    }
+}
